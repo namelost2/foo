f24fbb706b886bcc6ab0430a55391a367280b8f5
namelost2 <144593173+namelost2@users.noreply.github.com>
2025-01-06 21:12:50 +0100

    Update foo.dart

--- foo.dart
+++ foo.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 /*
 class="instapaper_body js" data-path="
-class="blockinstapaper_body ' id=x tabindex=1 onfocusin='document.body.innerHTML="hello lol"' " data-path="
+class="blockinstapaper_body ' id=x tabindex=1 onfocusin='document.body.innerHTML="hello lol"' " blockdata-path="
 */
 
 void main() {
